a347af41b1c44a1340d2e0e8f349424edd192ea4
ArifRh9185 <arifrh9185@gmail.com>
2026-01-26 05:46:39 +0700

    Fix: correct axios create syntax

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // Tambahkan /api di ujungnya agar tidak perlu tulis berulang kali
-  baseURL: (process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000") + "/api",
-});
+  baseURL: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api` : "http://localhost:5000/api",
+})
 
 // Tambahkan token ke setiap request otomatis
 api.interceptors.request.use((config) => {
